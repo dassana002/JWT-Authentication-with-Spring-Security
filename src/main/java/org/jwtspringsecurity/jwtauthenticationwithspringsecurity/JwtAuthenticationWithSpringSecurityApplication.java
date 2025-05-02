1bdc95cf8de864a1f9d3ebdb564c77c235d608e2
Dassana <dassana002@gmail.com>
2025-05-02 17:13:44 +0530

    Project init

---
+++ src/main/java/org/jwtspringsecurity/jwtauthenticationwithspringsecurity/JwtAuthenticationWithSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package org.jwtspringsecurity.jwtauthenticationwithspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthenticationWithSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtAuthenticationWithSpringSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/jwtspringsecurity/jwtauthenticationwithspringsecurity/JwtAuthenticationWithSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jwtspringsecurity.jwtauthenticationwithspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthenticationWithSpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

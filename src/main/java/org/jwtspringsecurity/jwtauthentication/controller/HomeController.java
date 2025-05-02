dd40c4cba8f2dd84a0171cad5e82dc1bbf449544
Dassana <dassana002@gmail.com>
2025-05-02 17:51:00 +0530

    Added SecurityConfig with stateless session management and basic authentication setup

--- src/main/java/org/jwtspringsecurity/jwtauthentication/controller/HomeController.java
+++ src/main/java/org/jwtspringsecurity/jwtauthentication/controller/HomeController.java
@@ -1,6 +1,7 @@
 package org.jwtspringsecurity.jwtauthentication.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class HomeController {
     @GetMapping
     public String home() {
         return "Hello World";
+    }
+
+    @PostMapping("/login")
+    public String login() {
+        return "user log in";
     }
 }
